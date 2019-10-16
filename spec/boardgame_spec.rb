6bee17e642aca56458e44fe3ddb0aa4427e82253
krisn64209 <krisnorris@gmail.com>
2019-10-16 10:57:40 +0100

    refactored tests to remove unneeded lines

--- spec/boardgame_spec.rb
+++ spec/boardgame_spec.rb
@@ -4,12 +4,10 @@ describe Dice do
 
   it 'can roll a dice' do 
     new_roll = Dice.new
-    new_roll.roll_confirm
     expect(new_roll.roll_confirm).to eq "Dice rolled!"
   end
   it 'can only roll between 1 and 6' do
     new_roll = Dice.new
-    new_roll.roll
     expect(new_roll.roll).to be_between(1, 6).inclusive
   end
   it 'can give me a random number from each dice roll' do
@@ -19,12 +17,10 @@ describe Dice do
   end
   it 'can roll multiple dice' do     
     new_roll = Dice.new
-    new_roll.roll_multiple(2)
     expect(new_roll.roll_multiple(2).length).to be 2
   end
   it 'can tell me the scores of the dice rolled' do
     new_roll = Dice.new
-    new_roll.roll_multiple(4)
     expect(new_roll.roll_multiple(4)).to all( be_an(Integer) )
   end
 end
